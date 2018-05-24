1d1c1c0a244a0985267f8c39cd971e0f85d6c0af
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-05-24 17:53:40 +0200

    Update protectfile.c

--- Protectfile/protectfile.c
+++ Protectfile/protectfile.c
@@ -4,3 +4,19 @@
  * key bits; this number may be changed by redefining a constant near 
  * the start of the file.
  */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <strings.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/vnode.h>
+
+#include "rijndael.h"
+#include "encrypt.c"
+
