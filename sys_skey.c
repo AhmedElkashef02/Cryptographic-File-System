ed04fb8b37e4de446600c192cb6b4a3893556c75
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-05-24 23:03:53 +0200

    Update sys_skey.c

--- sys_skey.c
+++ sys_skey.c
@@ -18,28 +18,31 @@ struct skey_args {
 };
 #endif
 
-int sys_skey(struct thread *td, struct skey_args *args)
-{
-  struct ucred *cred = td->td_proc->p_ucred;
-  uid_t id = cred->cr_uid;
+int sys_skey(struct thread *td, struct skey_args *args) {
+  struct ucred *cred = td->td_proc->p_ucred;    /* Get the process owner's identity. */
+  uid_t id = cred->cr_uid;                      /* Get the effective user id */
+  
   unsigned int k0 = args->k0;
   unsigned int k1 = args->k1;
   unsigned char key[KEYLENGTH(KEYBITS)];
+  
   bzero (key, sizeof (key));
   bcopy (&k0, &(key[0]), sizeof (k0));
   bcopy (&k1, &(key[sizeof(k0)]), sizeof (k1));
 
-  /** Both keys supplied were 0, so disable */
+  /* If k0 and k1 were 0, disable encryption and decryption */
   if (k0 == 0 && k1 == 0) {
     cred->k0 = 0;
     cred->k1 = 0;
     return 0;
   }
 
-  /** Sets key */
+  /* for good compatibaility between protect file and setkey system call
+   * we get k0,k1 and construct the big key, assign it to the user. */
   cred->k0 = k0;
   cred->k1 = k1;
 
+  /* for testing */
   printf("Key = ");
   for (int i = 0; i < sizeof (key); i++) {
     printf ("%02x", key[sizeof(key)-i-1]);
