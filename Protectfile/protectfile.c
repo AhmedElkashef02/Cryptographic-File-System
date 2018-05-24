5fbcec68ff0f1bcc9937d7cfc989ba91a861565c
Ahmed Elkashef <ahmedelkashef2012@gmail.com>
2018-05-24 19:25:02 +0200

    add protectfile

---
+++ Protectfile/protectfile.c
@@ -0,0 +1,6 @@
+/*
+ * This code calls the encryption and decryption functions that in turn 
+ * uses the Rijndael (AES) cipher.  The key length is hard-coded to 128 
+ * key bits; this number may be changed by redefining a constant near 
+ * the start of the file.
+ */
